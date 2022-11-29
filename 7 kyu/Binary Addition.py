a17a30f2916ca8e7b9fcf8944c69cb7f6f572422
Viktor Gorelov <redvivos@gmail.com>
2022-11-29 13:17:55 +0300

    Binary Addition

---
+++ 7 kyu/Binary Addition.py
@@ -0,0 +1,10 @@
+# Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done
+# before, or after the addition.
+# The binary number returned should be a string.
+
+def add_binary(a,b):
+    return str(bin(a + b))[2::]
+
+# Лучшее решение
+# def add_binary(a,b):
+#     return bin(a+b)[2:]
